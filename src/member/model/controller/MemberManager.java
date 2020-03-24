11fbadc6f7327091433ada9d4e500beb4a7242e3
USER <USER@DESKTOP-0QB96NK>
2020-03-24 11:53:56 +0900

    과제 ver2

--- src/member/model/controller/MemberManager.java
+++ src/member/model/controller/MemberManager.java
@@ -7,14 +7,15 @@ import member.model.vo.Member;
 public class MemberManager {
 	
 	private static Member[] m = new Member[10];
-	private static int ctn = 1;
+	private static int ctn = 0;
 	private Scanner sc = new Scanner(System.in);
 	
 	public MemberManager() {}
 	
 	public void insertMember() {
 		
-		do {
+		for(int i = 0; i<m.length; i++) {
+			
 			System.out.println("아이디 : ");
 			String userId = sc.next();
 			System.out.println("패스워드 : ");
@@ -28,16 +29,10 @@ public class MemberManager {
 			System.out.println("이메일 : ");
 			String email = sc.next();
 			
-			
-			for(int i = 0; i<m.length; i++) {
-				m[i] = new Member(userId, userPwd, userName, age, gender, email);
-				//m.length++;
-				ctn++;
-			}
-			
-			System.out.println("입력이 완료되었습니다. 메인 메뉴로 돌아갑니다.");
-		} while(true);
-	}
+			m[i] = new Member(userId, userPwd, userName, age, gender, email);
+				++ctn;
+			}System.out.println("입력이 완료되었습니다. 메인 메뉴로 돌아갑니다.");
+		}
 		
 	
 	public void searchId() {
@@ -48,7 +43,7 @@ public class MemberManager {
 			
 			for(int i = 0; i<m.length; i++) {
 				if(m[i].equals(userId)) {
-					//printone(m[i]);
+					printone(m[i]);
 				} else {
 					System.out.println("검색한 회원 정보가 존재하지 않습니다."); return;
 				}
@@ -66,7 +61,7 @@ public class MemberManager {
 			
 			for(int i = 0; i<m.length; i++) {
 				if(m[i].equals(userName)) {
-					//printone(m[i]);
+					printone(m[i]);
 				} else {
 					System.out.println("검색한 회원 정보가 존재하지 않습니다.");
 				} return;
@@ -82,7 +77,7 @@ public class MemberManager {
 			
 			for(int i = 0; i<m.length; i++) {
 				if(m[i].equals(email)) {
-					//printone(m[i]);
+					printone(m[i]);
 				} else {
 					System.out.println("검색한 회원 정보가 존재하지 않습니다.");
 				} return;
@@ -100,7 +95,7 @@ public class MemberManager {
 				if(m[i].equals(userId)) {
 					System.out.println("바꿀 비밀번호를 입력하세요 : ");
 					int rePwd = sc.nextInt();
-					//userPwd.setUserPwd(rePwd);
+					userPwd.setUserPwd(rePwd);
 					System.out.println("패스워드 수정이 완료되었습니다."); return;
 				} else {
 					System.out.println("수정할 회원이 존재하지 않습니다."); return;
@@ -110,9 +105,9 @@ public class MemberManager {
 		} while(true);
 	}
 	
-	//public void updateName()
+	public void updateName()
 	
-	//public void updateEmail()
+	public void updateEmail()
 	
 	public void deleteOne() {
 		do {
@@ -122,7 +117,7 @@ public class MemberManager {
 			for(int i = 0; i<m.length; i++) {
 				if(m[i].equals(userId)) {
 					m[i] = m[i+1];
-					//stn--;
+					stn--;
 				} else {
 					System.out.println("삭제할 회원 정보가 존재하지 않습니다." );
 				}
@@ -130,7 +125,7 @@ public class MemberManager {
 		}while(true);
 	}
 	
-	/*public void deleteAll() {
+	public void deleteAll() {
 			for(int i = 0; i<m.length; i++) {
 				System.out.println(m[i]);
 			}
@@ -145,6 +140,6 @@ public class MemberManager {
 		}
 	}
 	
-	public void printOne(Member m)*/
+	public void printOne(Member m)
 
 }
